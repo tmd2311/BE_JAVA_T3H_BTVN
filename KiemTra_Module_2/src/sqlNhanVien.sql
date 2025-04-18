9546a79ec5f7e79b0de3973382838c4bfe2a8c05
tmd2311 <dung.kayc@gmail.com>
2025-04-18 14:22:28 +0700

    HoanThanh

---
+++ KiemTra_Module_2/src/sqlNhanVien.sql
@@ -0,0 +1,8 @@
+create database employee;
+
+CREATE TABLE employee (
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          name VARCHAR(50),
+                          age INT,
+                          salary DOUBLE
+);
